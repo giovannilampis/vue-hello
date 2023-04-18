de764daa267fa7a1257d182854b9cd18367bd9af
giovannilampis <giolampis@hotmail.com>
2023-04-18 14:57:56 +0200

    add vue class to script.js; change vue cdn in html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict"
+
+const {createApp} = Vue 
+
+createApp({
+    data() {
+        return {
+            // variabili vue 3
+            message: "Hello VUE JS 3 !"
+        }
+    },
+    methods: {
+        //funzioni vue 3
+    }
+}).mount('#app')
