df6fc68ce49574a6693aadf1238f9427a421ed89
giovannilampis <giolampis@hotmail.com>
2023-04-18 16:22:41 +0200

    add a picture by taking it from vue js data

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ createApp({
     data() {
         return {
             // variabili vue 3
-            message: "Hello VUE JS 3 !"
+            message: "Hello VUE JS 3 !",
+            urlImageGio: "./images/giogiogio.jpeg",
+            pictureGio: "Giovanni Lampis' yellow picture"
         }
     },
     methods: {
